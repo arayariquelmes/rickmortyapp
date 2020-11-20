763d5835072b191143692688e96ae74f4f2429b8
Sebastián Araya <arayariquelmes@gmail.com>
2020-11-19 21:10:15 -0300

    Generar el molde de personajes en el DOM
    Generar la base del card de personaje en el html
    Ocultar de manera inicial el molde con d-block
    Renderizar clones del card mediante cloneNode
    Incluir la dependencia de sweetalert

--- js/init.js
+++ js/init.js
@@ -1,7 +1,29 @@
+
+window.mostrarPersonaje = function(){
+  console.log(this.personaje);  
+};
+
+window.mostrar = (personajes)=>{
+
+    const molde = document.querySelector(".molde-personaje");
+    const contenedor = document.querySelector(".contenedor");
+    for(let i=0; i < personajes.length; ++i){
+        let p = personajes[i];
+        let copia = molde.cloneNode(true);
+        copia.querySelector('.nombre-titulo').innerText = p.name;
+        copia.querySelector('.imagen-personaje').src = p.image;
+        copia.querySelector('.btn-personaje').personaje = p;
+        copia.querySelector('.btn-personaje').addEventListener('click', window.mostrarPersonaje);
+        contenedor.appendChild(copia);
+    }
+};
+//window->document->HtmlElement
 //Escuchando cuando se carga la página
 window.addEventListener('DOMContentLoaded', async ()=>{
+    //Retorna una promesa
+
     let respuesta = await axios.get("https://rickandmortyapi.com/api/character");
     let personajes = respuesta.data.results;
-    console.log(personajes);
+    window.mostrar(personajes);
     //TODO: Renderizar la lista de personajes en la pagina
 });
